70e889e6d54ada86343e732d7a46e2e7bd0d88e5
Diego de Angelo <dideangelo@gmail.com>
2020-01-14 23:35:07 -0300

    added EventTest class

---
+++ tests/Entity/EventTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Event;
+use PHPUnit\Framework\TestCase;
+
+class EventTest extends TestCase
+{
+	protected $event;
+
+	public function setUp()
+	{
+		$this->event = new Event();
+	}
+
+	public function testSetAndGetName()
+	{
+		$this->event->setName("Evento Teste");
+
+		$this->assertEquals("Evento Teste", $this->event->getName());
+	}
+
+	public function testSetAndGetDescription()
+	{
+		$this->event->setDescription("Este é um <b>evento</b>");
+
+		$this->assertEquals("Este é um evento", $this->event->getDescription());
+	}
+
+	public function testSetAndGetDate()
+	{
+		$date = new \Datetime();
+		$date->setDate(2020, 1, 15);
+
+		$this->event->setDate($date);
+
+		$this->assertEquals("2020-01-15", $this->event->getDate()->format('Y-m-d'));
+	}
+
+	public function testSetAndGetTime()
+	{
+		$time = new \Datetime();
+		$time->setTime(8, 30, 0);
+
+		$this->event->setTime($time);
+
+		$this->assertEquals("08:30:00", $this->event->getTime()->format('H:i:s'));
+	}
+
+	public function testSetAndGetStatus()
+	{
+		$this->event->setStatus(Event::STATUS_CANCELLED);
+
+		$this->assertEquals(0, $this->event->getStatus());
+
+		$this->event->setStatus(Event::STATUS_ACTIVE);
+
+		$this->assertEquals(1, $this->event->getStatus());
+	}
+
+	public function testgetStatusOptions()
+	{
+		$eventsByReflection = Event::getStatusOptions();
+		$eventsByConstants = [Event::STATUS_CANCELLED, Event::STATUS_ACTIVE];
+
+		$this->assertEquals(0, count(array_diff($eventsByReflection, $eventsByConstants)));
+	}
+}
