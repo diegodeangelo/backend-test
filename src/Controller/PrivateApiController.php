5490b795efd1f9858d7941738f05d3d029c665cb
Diego de Angelo <dideangelo@gmail.com>
2020-01-19 12:39:33 -0300

    added PrivateApiController class for authenticated controllers

---
+++ src/Controller/PrivateApiController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class PrivateApiController extends AbstractController
+{
+    
+}
