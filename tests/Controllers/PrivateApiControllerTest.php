07ead72d6550b6474208eb96078674b3d7acf3e7
Diego de Angelo <dideangelo@gmail.com>
2020-01-20 09:47:24 -0300

    modifications for test token from the jwt authenticator

--- tests/Controllers/PrivateApiControllerTest.php
+++ tests/Controllers/PrivateApiControllerTest.php
@@ -1,11 +1,41 @@
 <?php
+namespace App\Tests\Controller;
 
-namespace App\Tests;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
-use App\Controllers\PrivateApiController;
-use PHPUnit\Framework\TestCase;
-
-class PrivateApiControllerTest extends TestCase
+class PrivateAPiControllerTest extends WebTestCase
 {
-	
+	protected function createAuthenticatedClient($username = 'user', $password = 'password')
+	{
+	    $client = static::createClient();
+
+	    $client->request(
+			'POST',
+			'/api/login_check',
+			array(),
+			array(),
+			array('CONTENT_TYPE' => 'application/json'),
+			json_encode(array(
+				'username' => $username,
+				'password' => $password,
+			))
+		);
+
+	    $data = json_decode($client->getResponse()->getContent(), true);
+
+	    $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+	    return $client;
+	}
+
+	/**
+	 * test getPagesAction
+	 */
+	public function testGetPages()
+	{
+	    $client = $this->createAuthenticatedClient();
+	    $client->request('GET', '/api/event/all');
+	    // ... 
+	}
 }
